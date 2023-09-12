91a1520093cbf6243617782f8d8f80c51f12cb50
alexis miron <2240773@cegeplimoilou.ca>
2023-09-12 12:14:24 -0400

    mis projet du prof

--- src/main/java/tp1/ApplicationGraph.java
+++ src/main/java/tp1/ApplicationGraph.java
@@ -3,19 +3,13 @@ package tp1;
 
 import org.mariuszgromada.math.mxparser.License;
 
-/**
- * Contrôleur pour le Tp1 sur les formes A-15 420-203
- *
- * @author Julie.Freve màj Jocelyn
- *
- */
 public class ApplicationGraph  {
 	VuePrincipale vue;
 
 
 
 	public static void main(String[] args) {
-		License.iConfirmNonCommercialUse("Martin Simoneau");
+		License.iConfirmNonCommercialUse("Martin Simoneau");//ne pas enlever
 
 	}
 }
