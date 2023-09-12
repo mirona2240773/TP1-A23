2701e4fc2db748aa97b8e1ca464fee72b4af9eed
alexis miron <2240773@cegeplimoilou.ca>
2023-09-12 12:45:45 -0400

    ApplicationGraph

--- src/main/java/tp1/ApplicationGraph.java
+++ src/main/java/tp1/ApplicationGraph.java
@@ -1,15 +1,23 @@
 package tp1;
 
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 import org.mariuszgromada.math.mxparser.License;
 
-public class ApplicationGraph  {
+public class ApplicationGraph extends Application {
 	VuePrincipale vue;
 
 
 
 	public static void main(String[] args) {
 		License.iConfirmNonCommercialUse("Martin Simoneau");//ne pas enlever
+		launch(args);
+	}
 
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setScene();
+		primaryStage.show();
 	}
 }
